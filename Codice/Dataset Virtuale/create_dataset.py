a1622d65b484aa6e3f8aa4fd97369b717592309d
Dario Curreri <48800335+dariocurreri@users.noreply.github.com>
2019-11-25 15:57:46 +0100

    Codice per la creazione del dataset virtuale

---
+++ Codice/Dataset Virtuale/create_dataset.py
@@ -0,0 +1,128 @@
+import numpy
+import csv
+import constant
+import dataset_row
+
+
+def generate_negative_patient():
+    patient = dataset_row.patient()
+    generate_test_for_negative_patient(patient)
+    generate_POCT_for_negative_patient(patient)
+    if not (patient.is_test_negative() and patient.is_POCT_negative()):
+        if patient.POCT is constant.INCONCLUSIVE_POCT:
+            patient.set_total_IGA_below_threshold_for_negative_patient()
+            patient.set_TTG_igg_for_negative_patient()
+            patient.set_blood_tests_from_TTG_igg()
+        else:
+            # patient.POCT is constant.NEGATIVE_POCT
+            patient.set_total_IGA_above_threshold_for_negative_patient()
+            patient.set_TTG_iga_for_negative_patient()
+            patient.set_blood_tests_from_TTG_iga()
+        if patient.blood_tests == constant.POSITIVE_BLOOD_TEST:
+            patient.biopsy = constant.NEGATIVE_BIOPSY
+    return patient
+
+
+def generate_test_for_negative_patient(patient):
+    anemia_random_number = numpy.random.randint(1, 5)
+    patient.anemia = test_checker(anemia_random_number)
+    osteopenia_random_number = numpy.random.randint(1, 4)
+    patient.osteopenia = test_checker(osteopenia_random_number)
+    chronic_diarrhea_random_number = numpy.random.randint(1, 21)
+    patient.chronic_diarrhea = test_checker(chronic_diarrhea_random_number)
+    growth_failure_random_number = numpy.random.randint(1, 141)
+    patient.growth_failure = test_checker(growth_failure_random_number)
+    genetic_disorders_random_number = numpy.random.randint(1, 1001)
+    patient.genetic_disorders = test_checker(genetic_disorders_random_number)
+    celiac_mother_random_number = numpy.random.randint(1, 101)
+    patient.celiac_mother = test_checker(celiac_mother_random_number)
+
+
+def generate_POCT_for_negative_patient(patient):
+    poct_random_number = numpy.random.randint(1, 601)
+    # positive POCT is highly unlikely
+    if poct_random_number is 1:
+        patient.POCT = constant.INCONCLUSIVE_POCT
+    else:
+        patient.POCT = constant.NEGATIVE_POCT
+
+
+def generate_positive_patient():
+    patient = dataset_row.patient()
+    generate_test_for_positive_patient(patient)
+    generate_POCT_for_positive_patient(patient)
+    if patient.POCT is constant.INCONCLUSIVE_POCT:
+        patient.set_total_IGA_below_threshold_for_positive_patient()
+        patient.set_TTG_igg_for_positive_patient()
+        patient.set_blood_tests_from_TTG_igg()
+    else:
+        patient.set_total_IGA_above_threshold_for_positive_patient()
+        if patient.POCT is constant.POSITIVE_POCT:
+            patient.set_TTG_iga_for_positive_patient()
+        if patient.POCT is constant.NEGATIVE_POCT:
+            patient.set_TTG_iga_for_negative_patient()
+        patient.set_blood_tests_from_TTG_iga()
+    patient.biopsy = constant.POSITIVE_BIOPSY
+    return patient
+
+
+def generate_test_for_positive_patient(patient):
+    anemia_random_number = numpy.random.randint(1, 3)
+    patient.anemia = test_checker(anemia_random_number)
+    osteopenia_random_number = numpy.random.randint(1, 6)
+    if osteopenia_random_number < 3:
+        patient.osteopenia = constant.POSITIVE
+    else:
+        patient.osteopenia = constant.NEGATIVE
+    chronic_diarrhea_random_number = numpy.random.randint(1, 4)
+    patient.chronic_diarrhea = test_checker(chronic_diarrhea_random_number)
+    growth_failure_random_number = numpy.random.randint(1, 6)
+    patient.growth_failure = test_checker(growth_failure_random_number)
+    genetic_disorders_random_number = numpy.random.randint(1, 21)
+    patient.genetic_disorders = test_checker(genetic_disorders_random_number)
+    celiac_mother_random_number = numpy.random.randint(1, 19)
+    patient.celiac_mother = test_checker(celiac_mother_random_number)
+
+
+def generate_POCT_for_positive_patient(patient):
+    if patient.is_test_negative():
+        poct_random_number = numpy.random.randint(1, 601)
+        if poct_random_number is 1:
+            patient.POCT = constant.INCONCLUSIVE_POCT
+        else:
+            patient.POCT = constant.POSITIVE_POCT
+    else:
+        poct_random_number = numpy.random.randint(1, 11)
+        if poct_random_number is 1:
+            poct_random_number = numpy.random.randint(1, 601)
+            if poct_random_number is 1:
+                patient.POCT = constant.INCONCLUSIVE_POCT
+            else:
+                patient.POCT = constant.NEGATIVE_POCT
+        else:
+            patient.POCT = constant.POSITIVE_POCT
+
+
+def test_checker(num):
+    if num is 1:
+        return constant.POSITIVE
+    else:
+        return constant.NEGATIVE
+
+
+def create_dataset():
+    columns = ["Anemia", "Osteopenia", "Diarrea Cronica", "Mancata Crescita", "Disturbi Genetici", "Madre Celiaca",
+               "POCT", "IGA totali", "TTG igg", "TTG_iga", "Esami del sangue", "Biopsia"]
+    with open("dataset.csv", "w") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(columns)
+        for _ in range(0, 10000):
+            writer.writerow(generate_positive_patient().values())
+            mu, sigma = 100.00, 2.00
+            num_negative_patients = int(numpy.round(numpy.random.normal(mu, sigma)))
+            for _ in range(0, num_negative_patients):
+                writer.writerow(generate_negative_patient().values())
+
+
+if __name__ == '__main__':
+    create_dataset()
---
+++ Codice/Dataset Virtuale/dataset_row.py
@@ -0,0 +1,117 @@
+import numpy
+import scipy.stats
+import constant
+
+
+class patient:
+
+    def __init__(self):
+        self.anemia = numpy.NaN
+        self.osteopenia = numpy.NaN
+        self.chronic_diarrhea = numpy.NaN
+        self.growth_failure = numpy.NaN
+        self.genetic_disorders = numpy.NaN
+        self.celiac_mother = numpy.NaN
+        self.POCT = numpy.NaN
+        self.total_IGA = numpy.NaN
+        self.TTG_igg = numpy.NaN
+        self.TTG_iga = numpy.NaN
+        self.blood_tests = numpy.NaN
+        self.biopsy = numpy.NaN
+
+    def is_POCT_negative(self):
+        if self.POCT is constant.NEGATIVE_POCT:
+            return True
+        else:
+            return False
+
+    def is_test_negative(self):
+        if (self.anemia is 0) and (self.osteopenia is 0) and (self.chronic_diarrhea is 0) and \
+                (self.growth_failure is 0) and (self.genetic_disorders is 0) and (self.celiac_mother is 0):
+            return True
+        else:
+            return False
+
+    def set_total_IGA_above_threshold_for_negative_patient(self):
+        mu, sigma = 7.00, 2.00
+        self.total_IGA = numpy.random.normal(mu, sigma)
+        if self.total_IGA < constant.IGA_THRESHOLD:
+            self.total_IGA = constant.IGA_THRESHOLD
+        self.total_IGA = round(self.total_IGA, 2)
+
+    def set_total_IGA_above_threshold_for_positive_patient(self):
+        mu, sigma = 8.00, 2.00
+        self.total_IGA = numpy.random.normal(mu, sigma)
+        if self.total_IGA < constant.IGA_THRESHOLD:
+            self.total_IGA = constant.IGA_THRESHOLD
+        self.total_IGA = round(self.total_IGA, 2)
+
+    def set_total_IGA_below_threshold_for_negative_patient(self):
+        self.total_IGA = numpy.random.uniform(0, 0.25)
+        self.total_IGA = round(self.total_IGA, 2)
+
+    def set_total_IGA_below_threshold_for_positive_patient(self):
+        mu, sigma = 0.125, 1.00
+        self.total_IGA = numpy.random.normal(mu, sigma)
+        if self.total_IGA < 0:
+            self.total_IGA = 0.00
+        self.total_IGA = round(self.total_IGA, 2)
+
+    def set_TTG_igg_for_negative_patient(self):
+        mu, sigma = 2.00, 2.00
+        self.TTG_igg = numpy.random.normal(mu, sigma)
+        if self.TTG_igg < 0:
+            self.TTG_igg = 0.00
+        self.TTG_igg = round(self.TTG_igg, 2)
+
+    def set_TTG_igg_for_positive_patient(self):
+        mu, sigma = 14.00, 2.00
+        self.TTG_igg = numpy.random.normal(mu, sigma)
+        if self.TTG_igg < 0:
+            self.TTG_igg = 0.00
+        self.TTG_igg = round(self.TTG_igg, 2)
+
+    def set_TTG_iga_for_negative_patient(self):
+        mu, sigma = 4.50, 2.00
+        self.TTG_iga = numpy.random.normal(mu, sigma)
+        if self.TTG_iga < 0:
+            self.TTG_iga = 0.00
+        self.TTG_iga = round(self.TTG_iga, 2)
+
+    def set_TTG_iga_for_positive_patient(self):
+        a, mu, sigma = 0.50, 24.00, 2.00
+        self.TTG_iga = scipy.stats.skewnorm.rvs(a, mu, sigma)
+        if self.TTG_iga < 0:
+            self.TTG_iga = 0.00
+        self.TTG_iga = round(self.TTG_iga, 2)
+
+    def set_blood_tests_from_TTG_igg(self):
+        if self.TTG_igg < constant.TTG_IGG_THRESHOLD:
+            self.blood_tests = constant.NEGATIVE_BLOOD_TEST
+        else:
+            self.blood_tests = constant.POSITIVE_BLOOD_TEST
+
+    def set_blood_tests_from_TTG_iga(self):
+        if self.TTG_iga < constant.TTG_IGA_NEGATIVE_THRESHOLD:
+            self.blood_tests = constant.NEGATIVE_BLOOD_TEST
+        elif self.TTG_iga < constant.TTG_IGA_POSITIVE_THRESHOLD:
+            # borderline value
+            self.blood_tests = constant.NEGATIVE_BLOOD_TEST
+        else:
+            self.blood_tests = constant.POSITIVE_BLOOD_TEST
+
+    def values(self):
+        values = list()
+        values.append(self.anemia)
+        values.append(self.osteopenia)
+        values.append(self.chronic_diarrhea)
+        values.append(self.growth_failure)
+        values.append(self.genetic_disorders)
+        values.append(self.celiac_mother)
+        values.append(self.POCT)
+        values.append(self.total_IGA)
+        values.append(self.TTG_igg)
+        values.append(self.TTG_iga)
+        values.append(self.blood_tests)
+        values.append(self.biopsy)
+        return values
