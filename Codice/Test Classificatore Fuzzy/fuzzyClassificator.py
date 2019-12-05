5289ca95e383da24a820b3f1303271ad26a3ab14
Dario Curreri <48800335+dariocurreri@users.noreply.github.com>
2019-12-05 20:49:53 +0100

    v3

--- Codice/Test Classificatore Fuzzy/fuzzyClassificator.py
+++ Codice/Test Classificatore Fuzzy/fuzzyClassificator.py
@@ -4,32 +4,35 @@ import FuzzyClassificator as FC
 import fylearn.fuzzylogic as ff
 from sklearn.model_selection import train_test_split
 from fylearn.fuzzylogic import TriangularSet
-from fylearn.fuzzylogic import TrapezoidalSet
 from FCLogger import SetLevel
-from itertools import islice
 
 
-data_path = "../resource/dataset_virtuale.csv"
+data_path = "../resource/sick.csv"
 data = pd.read_csv(data_path)
+
+
+# for sick #
+data = data.drop("referral_source", axis=1)
+data = data.drop("TBG", axis=1)
+data = data.drop("TBG_measured", axis=1)
+data = data.replace(["F", "M", "t", "f", "?", "negative", "sick"], [0, 1, 1, 0, np.NaN, 0, 1])
+
+
 num_columns = data.shape[1] - 1
 
 
-def prepare_dataset_sick():
-    data.drop("referral_source", axis=1)
-    data.replace(["F", "M", "t", "f", "?", "negative", "sick"], [0, 1, 1, 0, np.nan, 0, 1])
-    for i in range(len(data.columns)):
-        data[data.columns[i]] = pd.to_numeric(data[data.columns[i]])
-    for i in range(len(data.columns)):
-        name_column = data.columns[i]
-        replace_with = data[name_column].mean()
-        data[name_column].fillna(replace_with, inplace=True)
+def prepare_dataset():
+    for column in data.columns:
+        data[column] = pd.to_numeric(data[column])
+        replace_with = data[column].mean()
+        data[column].fillna(replace_with, inplace=True)
 
 
 def to_fuzzy():
-    for column in data.columns:
+    for column in data.columns[0:-1]:
         x = np.unique(data[column])
-        if x[0] == 0 and x[1] == 1:
-            t = TrapezoidalSet(0, 1, 1, 1)
+        if 1 < len(x) < 4 and x.__contains__(1) and x.__contains__(0):
+            t = TriangularSet(0, 1, 1)
             data[column] = t(np.array(data[column]))
         else:
             max = ff.max(data[column])
@@ -40,12 +43,9 @@ def to_fuzzy():
 
 
 def split_dataset():
-    train, test = train_test_split(data, test_size=0.3, random_state=1)
+    train, test = train_test_split(data, test_size=0.3, random_state=0)
     train.to_csv("ethalons.dat", index=False)
-    x_test = test.drop(data.columns[-1], axis=1)
-    y_test = test[data.columns[-1]]
-    x_test.to_csv("candidates.dat", index=False)
-    y_test.to_csv("realClass.dat", index=False)
+    test.to_csv("candidates.dat", index=False)
 
 
 def train_classifier():
@@ -54,7 +54,6 @@ def train_classifier():
     FC.neuroNetworkFile = "network.xml"
     FC.sepSymbol = ","
     SetLevel("DEBUG")
-
     parameters = {
         "config": str(num_columns) + ",3,2,1",
         "epochs": 100,
@@ -71,7 +70,6 @@ def classify():
     FC.sepSymbol = ","
     FC.showExpected = True
     SetLevel("DEBUG")
-
     parameters = {
         "config": str(num_columns) + ",3,2,1",
         "epochs": 100,
@@ -81,8 +79,111 @@ def classify():
         "stop": 1
     }
     FC.Main(classifyParameters=parameters)
-    
 
+
+def evaluate_classifier(report_path):
+    TN = 0
+    TP = 0
+    FN = 0
+    FP = 0
+    counter = 0
+    med_counter = 0
+    med_positive = 0
+    med_negative = 0
+    with open(report_path, 'r') as report_file:
+        for line in report_file:
+            if line.__contains__("Output"):
+                counter += 1
+                line = line[line.find("Output"):]
+                if line.__contains__("Min") or line.__contains__("Low"):
+                    if line.__contains__("0"):
+                        TN += 1
+                    else:
+                        FN += 1
+                elif line.__contains__("Max") or line.__contains__("High"):
+                    if line.__contains__("1"):
+                        TP += 1
+                    else:
+                        FP += 1
+                else:
+                    med_counter += 1
+                    if line.__contains__("0"):
+                        med_negative += 1
+                    else:
+                        med_positive += 1
+    if med_negative > med_positive:
+        # consideriamo i valori med come target negativo
+        TN += med_negative
+        FN += med_positive
+    else:
+        TP += med_positive
+        FP += med_negative
+
+    print("Tot values classified " + str(counter))
+    print("Number of med values " + str(med_counter))
+    print("TN " + str(TN))
+    print("TP " + str(TP))
+    print("FN " + str(FN))
+    print("FP " + str(FP))
+
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    precision = TP / (TP + FP)
+    specificity = TN / (TN + FP)
+    recall = TP / (TP + FN)
+    print_report(accuracy, precision, specificity, recall)
+
+
+def print_report(acc, prec, spec, rec):
+    print("Accuracy: {0}%".format(acc * 100))
+    print("Precision: {0}%".format(prec * 100))
+    print("Specificity: {0}%".format(spec * 100))
+    print("Recall: {0}%\n".format(rec * 100))
+
+
+def menu():
+    print("\n\n-----------------------")
+    print("1. Learning Mode")
+    print("2. Classifying Mode")
+    print("3. Results")
+    print("0. Exit")
+    print("-----------------------")
+    temp = int(input())
+    print("\n")
+    return temp
+
+
+def op1():
+    prepare_dataset()
+    to_fuzzy()
+    data.to_csv("fuzzy.csv")
+    split_dataset()
+    train_classifier()
+
+
+def op2():
+    classify()
+
+
+def op3():
+    evaluate_classifier("report.txt")
+
+
+c = -1
+while c != 0:
+    c = menu()
+    if c == 1:
+        op1()
+    elif c == 2:
+        op2()
+    elif c == 3:
+        op3()
+    elif c == 0:
+        break
+    else:
+        print("Unknown command")
+
+
+"""
 def evaluate_classifier(realClass_path, report_path):
     Y_test = []
     Y_pred = []
@@ -160,54 +261,4 @@ def evaluate_classifier(realClass_path, report_path):
     recall = TP / (TP + FN)
 
     return accuracy, precision, specificity, recall
-
-
-def print_report(acc, prec, spec, rec):
-    print("Accuracy: {0}%".format(acc * 100))
-    print("Precision: {0}%".format(prec * 100))
-    print("Specificity: {0}%".format(spec * 100))
-    print("Recall: {0}%\n".format(rec * 100))
-
-
-def menu():
-    print("\n\n-----------------------")
-    print("1. Learning Mode")
-    print("2. Classifying Mode")
-    print("3. Results")
-    print("0. Exit")
-    print("-----------------------")
-    temp = int(input())
-    print("\n")
-    return temp
-
-
-def op1():
-    to_fuzzy()
-    split_dataset()
-    train_classifier()
-
-
-def op2():
-    classify()
-
-
-def op3():
-    realClass_path = 'realClass.dat'
-    report_path = 'report.txt'
-    acc, prec, spec, rec = evaluate_classifier(realClass_path, report_path)
-    print_report(acc, prec, spec, rec)
-
-
-c = -1
-while c != 0:
-    c = menu()
-    if c == 1:
-        op1()
-    elif c == 2:
-        op2()
-    elif c == 3:
-        op3()
-    elif c == 0:
-        break
-    else:
-        print("Unknown command")
+"""
